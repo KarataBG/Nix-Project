1e282d8772d56d37d51ffa0a12a3858647b3f12a
KarataBG <niki.karata@gmail.com>
2025-06-13 16:06:17 +0300

    Working checks for finding if it is python go or rust code

--- flake.nix
+++ flake.nix
@@ -17,20 +17,24 @@
       in
         { websiteSource = websiteSource; owner = owner; repo = repo; };
 
-      # Generate the package based on input parameters          
 
-      generatePackage = { url, rev, tag, option, hash }: let
+        
+
+
+      # Generate the package based on input parameters  
+      generatePackage = { url, rev, version, option, hash }: let
         parsed = parseGitHubUrl url;
 
         owner = parsed.owner;
         repo = parsed.repo;
 
         name = "${repo}-automated-package";
-        version = tag;
+        # version = version;
+        inherit version;
 
         src = pkgs.fetchFromGitHub {
-          inherit owner repo;
-          rev = tag;
+          inherit owner repo rev;
+          # rev = release;
           hash = hash;
         };
         package = {
@@ -40,83 +44,130 @@
             license = "MIT";
           };
         };
-      in
+
+        
+          # Detect the language based on common files
+          isPython = builtins.pathExists "${src}/setup.py"; 
+          isPyProject = builtins.pathExists "${src}/pyproject.toml";
+          isGo = builtins.pathExists "${src}/go.mod";
+          isRust = builtins.pathExists "${src}/Cargo.toml";
+
+          # isPyProject = builtins.hasAttr "pyproject.toml" src;
+          # isGo = builtins.hasAttr "go.mod" src;
+          # isRust = builtins.hasAttr "Cargo.toml" src; 
+      
+      in          
+
         if option == 1 then
           # Option 1: direct application build standard nix-2 packet
           # pkgs.python3Packages.buildPythonApplication {
-          pkgs.stdenv.mkDerivation rec {
 
 
-            buildInputs = with pkgs;[ python3 go rustc cargo ];
-            nativeBuildInputs = with pkgs;[ makeWrapper python3Packages.setuptools];
+        # inherit isPython isPyProject isGo isRust;
+
+          if isPython then
+            if isPyProject then pkgs.python3Packages.buildPythonApplication rec {
+              inherit src name version; 
+              pyproject = true;
+              dependencies = with pkgs.python3Packages; [
+              setuptools ply pillow
+            ];            
+            }
+            else pkgs.python3Packages.buildPythonApplication rec {
+              inherit src name version; 
+              pyproject = true;
+              dependencies = with pkgs.python3Packages; [
+              setuptools ply pillow
+            ];            
+            }
+          else if isGo then
+            pkgs.buildGoModule { inherit src name version; }
+          else if isRust then
+            pkgs.buildRustPackage { inherit src name version; }
+          else
+            throw "Unknown language or missing necessary build files. Please check your source structure."
+  
+
+
+
+
+
+
+
+
+
+
+
+          # pkgs.stdenv.mkDerivation rec {
+
+
+          #   buildInputs = with pkgs;[ python3 go rustc cargo ];
+          #   nativeBuildInputs = with pkgs;[ makeWrapper python3Packages.setuptools];
+
+          #   buildPhase = ''
+          #     ${pkgs.python3.interpreter} setup.py build
+          #   '';
+          #   installPhase = ''
+          #     ${pkgs.python3.interpreter} setup.py install
+          #   '';
+
+          #   buildPhase = ''
+          #     echo "Debug build phase..."
+
+          #     # Build Python package
+          #     if [ -d "python" ]; then
+          #     python3 setup.py build
+          #     python3 setup.py install --prefix=$out
+          #     echo "Python build complete."
+          #     fi
+
+          #     # Build Go package
+          #     if [ -d "go" ]; then
+          #       cd ${repo}
+          #       go build -o FILENAME
+          #     fi
+
+          #     # Build Rust package
+          #     if [ -d "rust" ]; then
+          #       # cd ${repo}
+          #       cargo build --release
+          #     fi
+          #   '';
+
+          #   installPhase = ''
+          #     mkdir -p $out/bin
+
+          #     # Python: If a Python binary is generated
+          #     if [ -f "${repo}" ]; then
+          #       cp ${repo} $out/bin/
+          #     else
+          #       echo "Python binary not found!"
+          #     fi
+
+          #     # Go: If the Go binary is generated
+          #     if [ -f "go/${repo}-automated-package" ]; then
+          #       cp go/${repo}-automated-package $out/bin/
+          #     else
+          #       echo "Go binary not found!"
+          #     fi
+
+          #     # Rust: If the Rust binary is generated
+          #     if [ -f "target/release/${repo}" ]; then
+          #       cp target/release/${repo} $out/bin/
+          #     else
+          #       echo "Rust binary not found!"
+          #     fi
+          #   '';
+
+          #   inherit (package) name version src meta;
+
+          #   # buildInputs = with pkgs.python3Packages; [setuptools ply pillow];
+          #   # installPhase = ''
+          #           # mkdir -p $out/bin
+          #           # cp -r $src/* $out/
+          #         # '';
             
-            buildPhase = ''
-              ${pkgs.python3.interpreter} setup.py build
-            '';
-            installPhase = ''
-              ${pkgs.python3.interpreter} setup.py install --prefix=$out
-            '';
-
-            # buildPhase = ''
-            #   echo "Debug build phase..."
-
-            #   # Build Python package
-            #   if [ -d "python" ]; then
-            #   python3 setup.py build
-            #   python3 setup.py install --prefix=$out
-            #   echo "Python build complete."
-            #   fi
-
-            #   # Build Go package
-            #   if [ -d "go" ]; then
-            #     cd ${repo}
-            #     go build -o FILENAME
-            #   fi
-
-            #   # Build Rust package
-            #   if [ -d "rust" ]; then
-            #     # cd ${repo}
-            #     cargo build --release
-            #   fi
-            # '';
-
-            # installPhase = ''
-            #   mkdir -p $out/bin
-
-            #   # Python: If a Python binary is generated
-            #   if [ -f "${repo}" ]; then
-            #     cp ${repo} $out/bin/
-            #   else
-            #     echo "Python binary not found!"
-            #   fi
-
-            #   # Go: If the Go binary is generated
-            #   if [ -f "go/${repo}-automated-package" ]; then
-            #     cp go/${repo}-automated-package $out/bin/
-            #   else
-            #     echo "Go binary not found!"
-            #   fi
-
-            #   # Rust: If the Rust binary is generated
-            #   if [ -f "target/release/${repo}" ]; then
-            #     cp target/release/${repo} $out/bin/
-            #   else
-            #     echo "Rust binary not found!"
-            #   fi
-            # '';
-
-            inherit (package) name version src meta;
-
-            # buildInputs = with pkgs.python3Packages; [setuptools ply pillow];
-            # installPhase = ''
-                    # mkdir -p $out/bin
-                    # cp -r $src/* $out/
-                  # '';
-            # pyproject = true;
-            # dependencies = with pkgs.python3Packages; [
-              # setuptools ply pillow
-            # ];
-          }
+          # }
         else if option == 2 then
           # Option 2: returns a flake with the package
           let
@@ -161,18 +212,21 @@
     in {
       packages.${system}.examplePackage1 = generatePackage {
         url = "https://github.com/LoLei/razer-cli";
-        rev = "4b979a4";
-        tag = "0.7.6";
-        hash = "sha256-jjCMxY0PEar9F4O4vu5niU2U74rxoaBczqW5CKLEKvk=";
+        rev = "9ce5688";
+        version = "2.3.0";
+        hash = "sha256-uwTqDCYmG/5dyse0tF/CPG+9SlThyRyeHJ0OSBpcQio=";
         option = 1;  # options - 1 2 3
       };
+
+      inherit (generatePackage) src isPython isPyProject isGo isRust;
     };
 }
 
+#ako podam rev wzima nmlc dori i drugite da sa na razer
 
 
 # sha256-uwTqDCYmG/5dyse0tF/CPG+9SlThyRyeHJ0OSBpcQio=
-# razer
+# razer 2.3.0
 
 # sha256-jAvzfmv8iLs4jb/rzRswiAPHZpx20hjfbG/NY4HGcF0= 
 # nmlc
