fa4a43dfbda2f7eea71363b3e77b887c89191c9a
KarataBG <niki.karata@gmail.com>
2025-06-14 15:48:23 +0300

    Fetches from the correct repo website and and added extraArgs for needed touch ups

--- flake.nix
+++ flake.nix
@@ -1,25 +1,29 @@
 {
+
   description = "Flake wrapping GitHub packages with options";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      inherit (nixpkgs) lib;
 
       # Parse GitHub URL for extracting owner/repo information
-      parseGitHubUrl = url: 
-      let
-        parts = pkgs.lib.strings.splitString "/" url;
-        websiteSource = builtins.elemAt parts 2;
-        owner = builtins.elemAt parts 3;
-        repo = builtins.elemAt parts 4;
-      in
-        { websiteSource = websiteSource; owner = owner; repo = repo; };
+      parseGitHubUrl = url:
+        let
+          parts = lib.strings.splitString "/" url;
+          websiteSource = builtins.elemAt parts 2;
+          owner = builtins.elemAt parts 3;
+          repo = builtins.elemAt parts 4;
+        in {
+          websiteSource = websiteSource;
+          owner = owner;
+          repo = repo;
+        };
 
-
-      URLParser = {url, rev ? "", version ? "1.0.0", hash }:
+      URLParser = { url, rev ? "", version ? "1.0.0", hash, }:
         let
           parsed = parseGitHubUrl url;
 
@@ -27,102 +31,136 @@
           repo = parsed.repo;
           websiteSource = parsed.websiteSource;
 
-          
-
           fetchGitHub = pkgs.fetchFromGitHub {
-            inherit owner repo hash rev version;
+            inherit owner repo hash;
+            tag = version;
           };
+
+          fetchGitLab =
+            pkgs.fetchFromGitLab { inherit owner repo hash rev version; };
+
+          fetchGitiles =
+            pkgs.fetchFromGitiles { inherit url hash rev version; };
+
+          fetchCrate = pkgs.fetchCrate {
+            pname = repo;
+            inherit hash version;
+          };
+        in if websiteSource == "github.com" then
+          fetchGitHub
+        else if websiteSource == "gitlab.com" then
+          fetchGitLab
+        else if websiteSource == "chromium.googlesource.com" then
+          fetchGitiles
+        else if websiteSource == "crates.io" then
+          fetchCrate
+        else
+          throw "Unsuported website";
+
+      # Generate the package based on input parameters
+      generatePackage = { url, rev ? "", version ? "1.0.0", option ? 1
+        , hash ? lib.trace "Use the generated hash as input" lib.fakeHash
+        , vendorHash ? pkgs.lib.trace "Use the generated vendorHash as input" lib.fakeHash
+        , extraArgs ? { } }:
+        let
+
+          src = URLParser {
+            url = url;
+            rev = rev;
+            version = version;
+            hash = hash;
+          };
+
+          repo = (parseGitHubUrl url).repo;
+          name = "${repo}-automated-package";
+
+          package = {
+            inherit name version src;
+            meta = {
+              description = "An automatic package";
+              license = "MIT";
+            };
+          };
+
+          # Detect the language based on common files
+          isPython = builtins.pathExists "${src}/setup.py";
+          isPyProject = builtins.pathExists "${src}/pyproject.toml";
+          isGo = builtins.pathExists "${src}/go.mod";
+          isRust = builtins.pathExists "${src}/Cargo.toml";
+          isCargoLock = builtins.pathExists "${src}/Cargo.lock";
+
+          rustCargoLock = if isRust && isCargoLock then
+            let fixupLockFile = path: (builtins.readFile path);
+            in { lockFileContents = fixupLockFile "${src}/Cargo.lock"; }
+          else
+            null;
+
           
-          fetchGitLab = pkgs.fetchFromGitLab {
-            inherit owner repo hash rev version;
-          };
 
-          fetchGitiles = pkgs.fetchFromGitiles {
-            inherit url hash rev version;
-          };
-
-          fetchCrate = pkgs.fetchFromGitiles {
-
-            inherit repo hash rev version;
-          };
-        in
-          if websiteSource == "github.com" then fetchGitHub
-          else if websiteSource == "gitlab.com" then fetchGitLab
-          else if websiteSource == "chromium.googlesource.com" then fetchGitiles
-          else if websiteSource == "creates.io" then fetchCrate
-          else throw "Unsuported website";
-
-      # Generate the package based on input parameters  
-      generatePackage = { url, rev ? "", version ? "1.0.0", option ? 1, hash, vendorHash ? null }: let
-        
-        src = URLParser { url = url; rev = rev; version = version; hash = hash; };
-
-        repo = (parseGitHubUrl url).repo;
-        name = "${repo}-automated-package";
-        
-        package = {
-          inherit name version src;
-          meta = {
-            description = "An automatic package";
-            license = "MIT";
-          };
-        };
-
-        
-        # Detect the language based on common files
-        isPython = builtins.pathExists "${src}/setup.py"; 
-        isPyProject = builtins.pathExists "${src}/pyproject.toml";
-        isGo = builtins.pathExists "${src}/go.mod";
-        isRust = builtins.pathExists "${src}/Cargo.toml";
-        isCargoLock = builtins.pathExists "${src}/Cargo.lock";
-
-        rustCargoLock = if isRust && isCargoLock then
-          let fixupLockFile = path: (builtins.readFile path);
-          in {lockFileContents = fixupLockFile "${src}/Cargo.lock";}
-        else null;
-      
-      in     
-
-        if option == 1 then
-          # Option 1: direct application build standard nix-2 packet
+        in if option == 1 then
+        # Option 1: direct application build standard nix-2 packet
 
           if isPython then
-            if isPyProject then pkgs.python3Packages.buildPythonApplication rec {
-              inherit src name version; 
-              pyproject = true;
-              dependencies = with pkgs.python3Packages; [
-              setuptools ply pillow
-            ];            
-            }
-            else pkgs.python3Packages.buildPythonApplication rec {
-              inherit src name version; 
-              dependencies = with pkgs.python3Packages; [
-              setuptools ply pillow
-            ];            
-            }
-          else if isGo then
-            pkgs.buildGoModule rec { 
-              inherit src name version vendorHash;
-              # modRoot = "cmd/jwx";
-
+            if isPyProject then
+              pkgs.python3Packages.buildPythonApplication rec {
+                inherit src name version;
+                pyproject = true;
+                dependencies = with pkgs.python3Packages; [
+                  setuptools
+                  ply
+                  pillow
+                ];
               }
+            else
+              pkgs.python3Packages.buildPythonApplication rec {
+                inherit src name version;
+                dependencies = with pkgs.python3Packages; [
+                  setuptools
+                  ply
+                  pillow
+                ];
+              }
+          else if isGo then
+
+            pkgs.buildGoModule rec {
+
+
+              inherit src name version vendorHash;
+
+              modRoot = if builtins.hasAttr "modRoot" extraArgs then
+                extraArgs.modRoot
+              else
+                "";
+
+              buildInputs = if builtins.hasAttr "buildInputs" extraArgs then
+                extraArgs.buildInputs
+              else
+                [ ];
+
+              doCheck = if builtins.hasAttr "doCheck" extraArgs then
+                extraArgs.doCheck
+              else
+                true;
+
+            }
           else if isRust then
-            pkgs.rustPlatform.buildRustPackage rec{ 
-              inherit src name version; 
+            pkgs.rustPlatform.buildRustPackage rec {
+              inherit src name version;
               cargoLock = rustCargoLock;
             }
           else
-            throw "Unknown language or missing necessary build files. Please check your source structure."
+            throw
+            "Unknown language or missing necessary build files. Please check your source structure."
 
         else if option == 2 then
-          # Option 2: returns a flake with the package
+        # Option 2: returns a flake with the package
           let
             packageFlake = {
               description = "A flake containing the package";
 
               inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-              outputs = { self, nixpkgs }: 
+              outputs = { self, nixpkgs }:
                 let
                   system = "x86_64-linux";
                   pkgs = import nixpkgs { inherit system; };
@@ -136,29 +174,28 @@
                   };
                 };
             };
-          in
-            packageFlake
+          in packageFlake
         else if option == 3 then
-          # Defines package for callPackage {}
+        # Defines package for callPackage {}
           let
-            packageFile = pkgs.stdenv.mkDerivation rec{
-            inherit (package) name version src meta;
-          };
-          in
-           packageFile
+            packageFile = pkgs.stdenv.mkDerivation rec {
+              inherit (package) name version src meta;
+            };
+          in packageFile
 
         else
           throw "Invalid option. Please choose 1, 2, or 3.";
 
     in {
-      #python 
+      inherit pkgs;
+      #python
       packages.${system} = {
         examplePackage1 = generatePackage {
           url = "https://github.com/OpenTTD/nml";
           # rev = "5295c19";
           # version = "0.7.6";
           hash = "sha256-jAvzfmv8iLs4jb/rzRswiAPHZpx20hjfbG/NY4HGcF0=";
-          option = 1;  # options - 1 2 3
+          option = 1; # options - 1 2 3
         };
 
         #rust
@@ -167,25 +204,48 @@
           # rev = "5295c19";
           # version = "0.7.6";
           hash = "sha256-eWcN/iK/ToufABi4+hIyWetp2I94Vy4INHb4r6fw+TY=";
-          option = 1;  # options - 1 2 3
+          option = 1; # options - 1 2 3
+        };
+        examplePackage5 = generatePackage {
+          url = "https://gitlab.com/kornelski/mandown";
+          rev = "9da94876";
+          # version = "v1.1.0";
+          hash = "sha256-wEv7h3Kl4EczmsY4FuGOvRgeGf0rgANhONhCKyu6zik=";
+          option = 1; # options - 1 2 3
+        };
+        examplePackage6 = generatePackage {
+          url = "https://crates.io/crates/petname";
+          # rev = "9da94876";
+          version = "3.0.0-alpha.2";
+          hash = "sha256-6gJkaHAhau2HKKwVa/FL1mZfC9IJkyORm5P8MzLnQ5Q=";
+          option = 1; # options - 1 2 3
         };
 
-      #go
+        #go
         examplePackage3 = generatePackage {
           url = "https://github.com/lestrrat-go/jwx";
           rev = "a68b08e";
           version = "v3.0.6";
           hash = "sha256-D3HhkAEW1vxeq6bQhRLe9+i/0u6CUhR6azWwIpudhBI=";
-          vendorHash = "sha256-JXH8wqf3CuqOB2t+tcM8pY7nS4LTpGWdgnJdaYYkXwU=";
-          option = 1;  # options - 1 2 3
+          vendorHash = "sha256-FjNUcNI3A97ngPZBWW+6qL0eCTd10KUGl/AzByXSZt8=";
+          # vendorHash = "sha256-JXH8wqf3CuqOB2t+tcM8pY7nS4LTpGWdgnJdaYYkXwU=";
+          option = 1; # options - 1 2 3
+          extraArgs = { modRoot = "cmd/jwx"; };
         };
         examplePackage4 = generatePackage {
           url = "https://github.com/cfoust/cy";
           rev = "77ea96a";
           version = "v1.5.1";
           hash = "sha256-lRBggQqi5F667w2wkMrbmTZu7DX/wHD5a4UIwm1s6V4=";
-          option = 1;  # options - 1 2 3
+          vendorHash = null;
+          option = 1; # options - 1 2 3
+          # extraArgs = with pkgs; { buildInputs = [ pkgs.xorg.libX11 ]; };
+          extraArgs = with pkgs; {
+            buildInputs = [ pkgs.xorg.libX11 ];
+            doCheck = false;
+          };
         };
+
       };
 
     };
@@ -194,7 +254,7 @@
 #python
 # sha256-uwTqDCYmG/5dyse0tF/CPG+9SlThyRyeHJ0OSBpcQio=
 # razer 2.3.0
-# sha256-jAvzfmv8iLs4jb/rzRswiAPHZpx20hjfbG/NY4HGcF0= 
+# sha256-jAvzfmv8iLs4jb/rzRswiAPHZpx20hjfbG/NY4HGcF0=
 # nmlc 0.7.6
 
 #rust
@@ -206,6 +266,5 @@
 # sha256-lRBggQqi5F667w2wkMrbmTZu7DX/wHD5a4UIwm1s6V4=
 # cy
 # sha256-D3HhkAEW1vxeq6bQhRLe9+i/0u6CUhR6azWwIpudhBI=
-# vendorHash = "sha256-JXH8wqf3CuqOB2t+tcM8pY7nS4LTpGWdgnJdaYYkXwU=";
+# vendorHash = "sha256-FjNUcNI3A97ngPZBWW+6qL0eCTd10KUGl/AzByXSZt8=";
 # jwx
-             
